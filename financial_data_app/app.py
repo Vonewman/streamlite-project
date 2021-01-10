245d4df07b4cb3a6ab8dcc9a53b411d7d4ed3a4f
vonewman <vonewman7@gmail.com>
2021-01-10 14:57:10 +0000

    Fist commit

---
+++ financial_data_app/app.py
@@ -0,0 +1,36 @@
+import yfinance as yf
+import streamlit as st
+
+
+st.write("""
+# Simple Stock Price App
+
+Shown are the **stock price** and ***volume*** of Google!
+
+Colons can be used to align columns.
+
+| Tables        | Are           | Cool  |
+| ------------- |:-------------:| -----:|
+| col 3 is      | right-aligned | $1600 |
+| col 2 is      | centered      |   $12 |
+| zebra stripes | are neat      |    $1 |
+
+""")
+
+
+# define the ticker symbol
+tickerSymbol = 'GOOGL'
+# get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+# get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start='2010-5-31',
+                              end='2020-5-31')
+
+st.write("""
+## Closing Price
+""")
+st.line_chart(tickerDf.Close)
+st.write("""
+## Volume Price
+""")
+st.line_chart(tickerDf.Volume)
